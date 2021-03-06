b91580cd37b533e78956cd493b2ef06ca773e6b1
Mika Le <le.mika03@gmail.com>
2021-03-06 12:58:31 -0500

    Documentation updated

--- src/Product.java
+++ src/Product.java
@@ -1,32 +1,51 @@
-/* Product class. Has accessor methods for all attributes.
- * Authors: Dhriti Aravind 101141942, Mika Le 101141818
- * Last edited: February 9, 2021
+/**
+ * A Product
+ * @author Dhriti Aravind 101141942, Mika Le 101141818
+ * @version 1.0
  */
-
 public class Product {
     private final String NAME;
     private final int ID;
     private final double PRICE;
 
+    /**
+     * Default constructor for a Product
+     */
     public Product() {
         this("",0,0);
     }
 
+    /**
+     * Constructor for a Product
+     * @param name  String, name of the product
+     * @param id    int, the product ID
+     * @param price double, the price of the product
+     */
     public Product(String name, int id, double price) {
         NAME = name;
         ID= id;
         PRICE = price;
     }
 
+    /**
+     * This method provides access to the name of the product
+     * @return String, name of the Product
+     */
     public String getName() {
-        return NAME;
+        return this.NAME;
     }
 
+    /**
+     * This method provides access to the ID of the product
+     * @return int, id of the Product
+     */
     public int getId() {
-        return ID;
+        return this.ID;
     }
 
-    public double getPrice() {
-        return PRICE;
-    }
+    /**
+     * This method provides access to the price of the product
+     * @return double, price of the Product
+     */
+    public double getPrice() { return this.PRICE; }
 }
