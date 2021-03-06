bf55789d0daede1959424bef57b3076280a29d18
Mika Le <le.mika03@gmail.com>
2021-03-06 12:58:49 -0500

    Shopping Cart Class v.1

--- src/ShoppingCart.java
+++ src/ShoppingCart.java
@@ -1,2 +1,12 @@
-public class ShoppingCart {
+/**
+ * A ShoppingCart
+ * @author Mika Le 101141818
+ */
+public class ShoppingCart extends Inventory{
+    /**
+     * Constructor for a ShoppingCart
+     */
+    public ShoppingCart(){
+        super();
+    }
 }
