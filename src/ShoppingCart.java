d5287f252118a6a0bcaf145a881f95ed63e86608
Mika Le <le.mika03@gmail.com>
2021-03-08 14:35:33 -0500

    added id

--- src/ShoppingCart.java
+++ src/ShoppingCart.java
@@ -2,11 +2,18 @@
  * A ShoppingCart
  * @author Mika Le 101141818
  */
+
 public class ShoppingCart extends Inventory{
+    private int id;
     /**
      * Constructor for a ShoppingCart
      */
-    public ShoppingCart(){
+    public ShoppingCart(int id){
         super();
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
     }
 }
